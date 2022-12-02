84a19b97903ece5804640cb4ec86c95a5e8e82ca
Andrea Perugini <andrea.perugini@unil.ch>
2022-12-02 01:17:39 +0100

    removed old files

--- merge_variants.sh
+++ merge_variants.sh
@@ -13,3 +13,14 @@ paste $file1 $namefile > $VAR3
 sleep 1m
 wait
 done
+
+for namefile in *.varorder.tsv;
+# to create a file in the proper format for ldsr
+do
+echo $namefile
+VAR3="$name$namefile"
+echo $VAR3
+paste $file1 $namefile > $VAR3
+sleep 1m
+wait
+done
