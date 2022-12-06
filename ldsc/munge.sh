f6e9388217b94e6d776e9d926fa362de051959bf
Andrea Perugini <andrea.perugini@unil.ch>
2022-12-06 21:53:06 +0100

    minor formatting changes

--- ldsc/munge.sh
+++ ldsc/munge.sh
@@ -56,12 +56,12 @@ done
 echo "Munging WMH BIG40 total volume IDP"
 
 ./munge_sumstats.py \
-	--sumstats /HDD/data/andrea/WMH_BIG40/volume_big40.txt \
-	--a1 a2 \
-	--a2 a1 \
-	--out /HDD/data/andrea/munged/brain/volume_big40 \
-	--chunksize 500000 \
-	--merge-alleles /HDD/data/andrea/w_hm3.snplist
+    --sumstats /HDD/data/andrea/WMH_BIG40/volume_big40.txt \
+    --a1 a2 \
+    --a2 a1 \
+    --out /HDD/data/andrea/munged/brain/volume_big40 \
+    --chunksize 500000 \
+    --merge-alleles /HDD/data/andrea/w_hm3.snplist
 
 #### PHENOTYPES #################################
 
